5d19a6901c9b6d189d79c7c262007209d704b1cd
sqlpadawan <sqlpadawan@gmail.com>
2026-09-10 12:52:57 -0400

    pivot to pico 1 w board

--- src/blink_bare/main.c
+++ src/blink_bare/main.c
@@ -14,16 +14,45 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#if defined(CYW43_WL_GPIO_LED_PIN)
+#include "pico/cyw43_arch.h"
+#elif !defined(PICO_DEFAULT_LED_PIN)
+#warning "Neither PICO_DEFAULT_LED_PIN nor CYW43_WL_GPIO_LED_PIN is defined for this board"
+#endif
+
+static bool led_init(void) {
+#if defined(CYW43_WL_GPIO_LED_PIN)
+    return cyw43_arch_init() == 0;
+#elif defined(PICO_DEFAULT_LED_PIN)
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return true;
+#else
+    return false;
+#endif
+}
+
+static void led_set(bool led_on) {
+#if defined(CYW43_WL_GPIO_LED_PIN)
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#elif defined(PICO_DEFAULT_LED_PIN)
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#else
+    (void)led_on;
+#endif
+}
+
 int main(void) {
     stdio_init_all();
 
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    if (!led_init()) {
+        printf("LED init failed\n");
+    }
 
     bool led_on = false;
     for (;;) {
         led_on = !led_on;
-        gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+        led_set(led_on);
         printf("bare LED %s\n", led_on ? "ON" : "OFF");
         sleep_ms(500);
     }
